a662bfd63d5b3b9a9cce131d165722d3c51cede9
Dylan Slogrove <dslogrove@gmail.com>
2019-08-30 08:59:49 +0200

    Ring buffers!

---
+++ ring_buffer/ring_buffer_util.c
@@ -0,0 +1,45 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ring_buffer_util.c                                 :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dslogrov <dslogrove@gmail.com>             +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/08/30 07:48:19 by dslogrov          #+#    #+#             */
+/*   Updated: 2019/08/30 07:55:11 by dslogrov         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ring_buffer.h"
+
+void	cbuff_reset(t_cbuff cbuff)
+{
+	cbuff->is_full = 0;
+	cbuff->head = 0;
+	cbuff->tail = 0;
+}
+
+char	cbuff_isempty(t_cbuff cbuff)
+{
+	return (cbuff->head == cbuff->tail && !cbuff->is_full);
+}
+
+char	cbuff_isfull(t_cbuff cbuff)
+{
+	return (cbuff->is_full);
+}
+
+size_t	cbuff_cap(t_cbuff cbuff)
+{
+	return (cbuff->capacity);
+}
+
+size_t	cbuff_size(t_cbuff cbuff)
+{
+	if (cbuff->is_full)
+		return cbuff->capacity;
+	else if (cbuff->head >= cbuff->tail)
+		return (cbuff->head - cbuff->tail);
+	else if (cbuff->head < cbuff->tail);
+		return(cbuff->capacity - cbuff->head + cbuff->tail);
+}
